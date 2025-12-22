145b8ce1c79b6760e4dce748af9220eb12bc1295
Kizzuato <kyyajuara@gmail.com>
2025-12-23 01:55:40 +0700

    menambahkan fitur map

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,13 @@
-import "./bootstrap";
-import L from "leaflet";
-import "leaflet/dist/leaflet.css";
+// import "./bootstrap";
+// import L from "leaflet";
+// import "leaflet/dist/leaflet.css";
 
-import icon from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
+// import icon from "leaflet/dist/images/marker-icon.png";
+// import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
-delete L.Icon.Default.prototype._getIconUrl;
+// delete L.Icon.Default.prototype._getIconUrl;
 
-L.Icon.Default.mergeOptions({
-    iconUrl: icon,
-    shadowUrl: iconShadow,
-});
+// L.Icon.Default.mergeOptions({
+//     iconUrl: icon,
+//     shadowUrl: iconShadow,
+// });
